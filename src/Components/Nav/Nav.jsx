65f54ca71fa715d6eeeee7e28aa36b8c92d5d5d1
Arian <arianmrv12@gmail.com>
2023-10-06 20:04:18 -0500

    return home fix

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -6,12 +6,13 @@ import "./Nav.css";
 
 const Nav = () => {
     const reload = () => {
-        window.location.reload();
+        if(window.location.pathname === "/mate-proyecto-2")
+            window.location.reload();
     }
     return (
         <>
             <div className="nav__content">
-                <Link to="/mate-proyecto-2"><RiHome2Line className="nav__content-icon"/></Link>
+                <Link to="/mate-proyecto-2" onClick={reload}><RiHome2Line className="nav__content-icon"/></Link>
                 <Link to="/mate-proyecto-2/integrantes"><BiUser className="nav__content-icon" id="users"/></Link>
             </div>
 
