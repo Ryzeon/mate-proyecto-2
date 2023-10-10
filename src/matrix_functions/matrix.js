762cb3ef41f639d7d4e4092eac90f8cb1ae4c21a
Ryzeon <alex.ramon2004@gmail.com>
2023-10-09 23:51:52 -0500

    Add dijkstra algotihm

--- src/matrix_functions/matrix.js
+++ src/matrix_functions/matrix.js
@@ -2,17 +2,17 @@ export const generateRandom = (n) => {
     const min_value = 1;
     const max_value = 9;
     const matrix = [];
-    for(let i=0; i<n; ++i) {
+    for (let i = 0; i < n; ++i) {
         const row = [];
-        for(let j=0; j<n; ++j) {
+        for (let j = 0; j < n; ++j) {
             row.push(0);
         }
         matrix.push(row);
     }
 
-    for(let i=0; i<n; ++i) {
-        for(let j=0; j<n; ++j) {
-            if(i===j) continue;
+    for (let i = 0; i < n; ++i) {
+        for (let j = 0; j < n; ++j) {
+            if (i === j) continue;
             matrix[i][j] = Math.floor(Math.random() * (max_value - min_value + 1)) + min_value;
             matrix[j][i] = matrix[i][j];
         }
@@ -23,9 +23,9 @@ export const generateRandom = (n) => {
 
 export const matrixInput = (n) => {
     const matrix = [];
-    for(let i=0; i<n; ++i) {
+    for (let i = 0; i < n; ++i) {
         const row = [];
-        for(let j=0; j<n; ++j) {
+        for (let j = 0; j < n; ++j) {
             row.push(0);
         }
         matrix.push(row);
@@ -33,12 +33,46 @@ export const matrixInput = (n) => {
     return matrix
 }
 
-/**
- *
- * @param matrix as simetric matriz
- * @param n -> size of matrix
- * @param start -> start node
- */
-export const dijkstra = (matrix, n, start) => {
-    console.log("ola uwuw test")
-}
+export const dijkstra_min = (matrix, n, src, target) => {
+    const dist = dijkstra(matrix, n, src);
+
+    if (dist[target] !== Infinity) {
+        return dist[target];
+    } else {
+        return -1;
+    }
+};
+
+export const dijkstra = (matrix, n, src) => {
+    const dist = Array(n).fill(Infinity);
+    const visited = Array(n).fill(false);
+
+    dist[src] = 0;
+
+    for (let i = 0; i < n; ++i) {
+        const u = findMinDistVertex(dist, visited);
+        visited[u] = true;
+
+        for (let v = 0; v < n; ++v) {
+            if (!visited[v] && matrix[u][v] !== 0) {
+                dist[v] = Math.min(dist[v], dist[u] + matrix[u][v]);
+            }
+        }
+    }
+
+    return dist;
+};
+
+const findMinDistVertex = (dist, visited) => {
+    let minDist = Infinity;
+    let minIndex = -1;
+
+    for (let i = 0; i < dist.length; ++i) {
+        if (!visited[i] && dist[i] < minDist) {
+            minDist = dist[i];
+            minIndex = i;
+        }
+    }
+
+    return minIndex;
+};
